e1503805eb62a04a792710d7840ea3512e919314
Vanry <ivanry@163.com>
2021-12-03 21:14:19 +0800

    only log internal server error

--- app/Handlers/ErrorHandler.php
+++ app/Handlers/ErrorHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Handlers;
 
+use Fig\Http\Message\StatusCodeInterface;
 use Slim\Handlers\ErrorHandler as Handler;
 
 class ErrorHandler extends Handler
@@ -10,7 +11,7 @@ class ErrorHandler extends Handler
 
     protected function logError(string $error): void
     {
-        if ($this->statusCode >= static::SERVER_ERROR_STATUS_CODE) {
+        if ($this->statusCode == StatusCodeInterface::STATUS_INTERNAL_SERVER_ERROR) {
             $this->logger->error($error);
         }
     }
