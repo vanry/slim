f9929b084efc8a88c5f30a67e8f27530fbf4140e
Vanry <ivanry@163.com>
2021-12-03 20:20:21 +0800

    修改默认模板内容

--- resources/views/index.php
+++ resources/views/index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Slim</title>
+    <title><?= config('app.name') ?></title>
 </head>
 <body>
-    <h1>Hello Slim</h1>
+    <h1>Slim Skeleton</h1>
 </body>
 </html>
