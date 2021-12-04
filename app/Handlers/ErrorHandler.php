bf6832935249976c77c109a234383d1faadada7e
Vanry <ivanry@163.com>
2021-12-04 14:17:33 +0800

    remove SERVER_ERROR_STATUS_CODE constant

--- app/Handlers/ErrorHandler.php
+++ app/Handlers/ErrorHandler.php
@@ -7,8 +7,6 @@ use Slim\Handlers\ErrorHandler as Handler;
 
 class ErrorHandler extends Handler
 {
-    const SERVER_ERROR_STATUS_CODE = 500;
-
     protected function logError(string $error): void
     {
         if ($this->statusCode == StatusCodeInterface::STATUS_INTERNAL_SERVER_ERROR) {
